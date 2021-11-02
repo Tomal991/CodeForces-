a2446275da13ac9634b1861bad43f1f597451642
Tomal991 <intesarulhaquey4@gmail.com>
2021-11-02 13:57:02 +0600

    228A

---
+++ 228A.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int color[4],count=0;
+    for(int i=0; i<4; i++)
+    {
+        cin>>color[i];
+    }
+    sort(color,color+4);
+    for(int i=0; i<3; i++)
+    {
+        if(color[i]==color[i+1])
+        {
+            count++;
+        }
+    }
+    cout<<count;
+
+}
